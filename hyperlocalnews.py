142f956518d3807d65a377511aa5768b48a1a008
san.io <73436163+san-io-aver@users.noreply.github.com>
2025-07-18 19:52:05 +0530

    Update hyperlocalnews.py

--- hyperlocalnews.py
+++ hyperlocalnews.py
@@ -9,6 +9,7 @@ st.title("City News with Sentiment Scores")
 city = st.text_input("Enter city name:", "delhi").strip().lower()
 news_amt = st.slider("Articles Quantity:", 1, 20, 5)
 
+
 citySites = {
     "TOI": {
         "url": f"https://timesofindia.indiatimes.com/city/{city}",
@@ -21,6 +22,17 @@ citySites = {
         "article_selector": "a",
         "filter": lambda href: f"{city}-news" in href,
         "base_url": "https://www.ndtv.com"
+    },
+    "INDEXP": {
+        "url": f"https://indianexpress.com/section/cities/{city}/",
+        "article_selector": "a",
+        "filter": lambda href: f"article/cities/" in href,
+        "base_url": "https://indianexpress.com"
+    },"TRIBUNE": {
+        "url": f"https://www.tribuneindia.com/news/city/{city}",
+        "article_selector": "a",
+        "filter": lambda href: f"/news/{city}/" in href,
+        "base_url": "https://www.tribuneindia.com"
     }
 }
 
@@ -37,7 +49,7 @@ def fetch_news(data):
                 full_link = href if href.startswith("http") else data["base_url"] + href
                 articles.append((title, full_link))
 
-        return articles[:news_amt]  # 10 news
+        return articles[:news_amt]  
     except Exception as e:
         return [(f"Error scraping: {e}", "")]
 
@@ -56,7 +68,7 @@ def sentiment_label(score):
 
 
 if city:
-    for name, data in citySites.items():
+    for name, data in citySites.items():    
         st.subheader(f"{name}")
         articles = fetch_news(data)
         cols = st.columns(3)
@@ -67,6 +79,17 @@ if city:
                     score = analyzer.polarity_scores(title)['compound']
                     label = sentiment_label(score)
                     title = shorten_text(title, 10)
-                    st.markdown(f"**{title}**")
-                    st.markdown(f"[Read more]({link})")
-                    st.write(f"Sentiment: {label}")            
+
+                    card_height = 200  
+
+                    st.markdown(
+                        f"""
+                        <div style='height: {card_height}px; overflow: hidden;'>
+                            <p style='font-weight: bold; font-size: 16px'>{title}</p>
+                            <p><a href='{link}' target='_blank'>Read more</a></p>
+                            <p>Sentiment: {label}</p>
+                        </div>
+                        """,
+                        unsafe_allow_html=True
+                    )
+           
